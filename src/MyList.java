0469c0fdeed587b5189e090477bfdb6a4f038e6d
ZhongLingYun <lingyun.zhong@hand-china.com>
2018-11-01 10:44:12 +0800

    改为泛型数组

--- src/MyList.java
+++ src/MyList.java
@@ -2,11 +2,11 @@ import java.util.Arrays;
 
 /**
  * @author ZhongLingYun
- * @Title: MyList
+ * @Title: MyList 修改为泛型数组
  * @Description: list
  * @date 2018/10/319:14
  */
-public class MyList {
+public class MyList<E> {
 
     /**
     * 数组总容量
@@ -20,15 +20,15 @@ public class MyList {
 
 
     /**
-     * 底层数组定义
+     * 底层数组定义 Java中没有泛型数组，只能通过object数组强转
      * */
-    private int[] listArray= new int[count];
+    private E[] listArray= (E[]) new Object[count];
 
     /**
      * 添加数据方法
      * 添加锁可以保证并发过程数据安全，但
      * */
-    public synchronized void add(int x){
+    public synchronized void add(E x){
         // 实现系统数组扩容
         listArray=createNewArray(listArray,size);
         listArray[size]=x;
@@ -45,14 +45,14 @@ public class MyList {
     /**
      * 获取某个数据
      * */
-    public int get(int x){
+    public E get(int x){
         return listArray[x];
     }
 
     /**
      * 底层数组扩容
      * */
-    private int[] createNewArray(int[] array, int s){
+    private E[] createNewArray(E[] array, int s){
         double x = 0;
         if(s>0){
             // 实际存储的长度/数组定义的长度，当实际存储的长度达到数组自定义的75%就扩容，每次扩容为 原来的1.5
@@ -68,20 +68,23 @@ public class MyList {
     /**
      * 删除某一个位置的元素
      * */
-    public int remove(int x){
-        int ret=listArray[x];
+    public E remove(int x){
+        E ret=listArray[x];
         for (int i=x;i<size;i++){
             listArray[i]=listArray[i+1];
         }
         size--;
+        // 确保Java回收机制可以回收
+        listArray[size]=null;
         return ret;
     }
 }
 
 class d{
     public static void main(String[] args) {
-        MyList  myList= new MyList();
-        for (int x=0;x<3;x++){
+        //  采用泛型数组 泛型 中不能有基本数据类型
+        MyList<Integer>  myList= new MyList();
+        for (int x=0;x<10;x++){
             myList.add(x);
            // System.out.println(myList.length());
         }
