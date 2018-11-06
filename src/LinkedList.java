190c4ff662d543c68ba90713ffbf8670d9cf9550
ZhongLingYun <lingyun.zhong@hand-china.com>
2018-11-06 23:42:26 +0800

    使用虚拟头部,需要实现链表的随机插入

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,115 +1,111 @@
 /**
  * @author ZhongLingYun
  * @Title: LinkedList
- * @Description: 链表
- * @date 2018/11/69:53
+ * @Description:
+ * @date 2018/11/622:33
  */
 public class LinkedList<E> {
-
     private class Node{
         private E e;
-        private Node next;
 
-        public Node(E e,Node next){
-            this.e=e;
-            this.next=next;
-        }
+        private Node next;
 
         public Node(E e){
             this.e=e;
             this.next=null;
         }
+
+        public Node(E e,Node node){
+            this.next=node;
+            this.e=e;
+        }
     }
 
-    private int size;
+    private int size=0;
 
     /**
-     * 虚拟头结点，不存储数据，只存储下一个节点
+     * 虚拟的头结点
      * */
     private Node dummyHead=new Node(null,null);
 
     /**
-     * 任意索引位置添加
+     * 从头部插入数据
      * */
-    public void add(E e,int position){
-        if(position<0||position>size){
-            // 可以在尾部插入
-            throw new RuntimeException("索引越界");
-        }
-            Node point=dummyHead;
-            for(int x=0;x<position;x++){
-                point=point.next;
-            }
-            // 在1234567中的3的位置插入i,2的后面就是i，i的后面就是3其余的不变
-           /* Node node= new Node(e);
-            point.next=node;
-            node.next=point.next;*/
-            //Node node=new Node(e,point.next);
-            //point.next=node;
-            point.next=new Node(e,point.next);
-            size++;
+    public void addAtHead(E e){
+        // 假设null,1,2,3,4在头部插入i变为null,i,1,2,3,4
+        dummyHead.next=new Node(e,dummyHead.next);
+        size++;
     }
 
     /**
-     * 获取任意位置的元素
+     * 在链表尾部插入
      * */
-    public E get(int position){
-        if(position<0||position>=size){
-            throw new RuntimeException("索引越界");
-        }
-        Node point=dummyHead;
-        for (int x=0;x<position-1;x++){
+    public void addAtEnd(E e){
+        Node point =dummyHead;
+        for(int x=0;x<size;x++){
             point=point.next;
         }
-        E e=point.next.e;
-        // 如果被取出来的是最后一个就没有下一个节点，下一个节点就是null
-        if(point.next.next==null){
-            point.next=null;
-        }else {
-            point.next=point.next.next;
+        point.next=new Node(e);
+        size++;
+    }
+
+    // TODO 随机插入
+
+    /**
+     * 从头部取出一个元素
+     * */
+    public E getAtHead(){
+        if(size==0){
+            throw  new RuntimeException("链表中没有数据");
         }
+        E e=dummyHead.next.e;
+        dummyHead.next=dummyHead.next.next;
+        size--;
         return e;
     }
 
     /**
-     * 头部添加
+     * 从尾部取出一个数据
      * */
-    public void add(E e){
-        // 在头部添加只要把原来头部的node作当前node下一个节点就可以
-        /*Node node= new Node(e);
-        node.next=head;
-        head=node;*/
-        add(e,0);
-    }
-
-    public E get(){
-        return get(0);
+    public E getAtEnd(){
+        if(size==0){
+            throw  new RuntimeException("链表中没有数据");
+        }
+        Node point=dummyHead;
+        for(int x=0;x<size-1;x++){
+           point=point.next;
+        }
+        E e=point.next.e;
+        point.next=null;
+        size--;
+        return e;
     }
 
     /**
-     * 当前链表的实际数据量
+     *链表是否为空
      * */
-    public int getSize(){
-        return size;
-    }
-
     public boolean isEmpty(){
         return size==0;
+    }
+
+    /**
+     * 链表的长度
+     **/
+    public int getSize(){
+        return size;
     }
 }
 
 class TestLinkedList{
-    public static void main(String[] args) {
-        LinkedList<Integer> linkedList= new LinkedList<>();
-        for (int x=0;x<3;x++){
-            linkedList.add(x);
+    public static void main(String[] args) throws InterruptedException {
+        LinkedList<Integer> linkedList=new LinkedList<>();
+        for (int x=0;x<10;x++){
+            linkedList.addAtHead(x);
         }
-        linkedList.add(33333,3);
-        for (int x=0;x<linkedList.getSize();x++){
-            System.out.println(linkedList.get());
+        for (int x=0;x<11;x++){
+            Thread.sleep(100);
+            System.out.println(linkedList.getAtEnd());
         }
-
-
 
     }
 }
